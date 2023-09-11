848ccd976d9aa8f0ce88f0f10bfffead2b2c64fe
Jahangir Hossain <jahangir758505@gmail.com>
2023-09-12 00:37:16 +0600

    update

--- components/Nav.js
+++ components/Nav.js
@@ -34,10 +34,12 @@ const Nav = () => {
   return <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
     <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-80 bg-white/10 xl:h-max py-8 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
       {navData.map((menu, index) => (
-        <Link className={`${menu.path === pathName && 'text-accent'} relative flex items-center group hover:text-accent transition-all duration-300`} key={index} href={menu.name}>
+        <Link className={`${menu.path === pathName && 'text-accent'} relative flex items-center group hover:text-accent transition-all duration-300`} key={index} href={menu.path}>
           <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
             <div className='bg-white relative flex text-primary items-center p-[6px] rounded-[3px]'>
               <div className='text-[12px] leading-none font-semibold capitalize'>{menu.name}</div>
+              {/* triangle */}
+              <div className='border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2'></div>
             </div>
           </div>
           <div>{menu.icon}</div>
