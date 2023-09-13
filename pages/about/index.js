5476d5aac94f412caa8de4269ada754200a950e9
Jahangir Hossain <jahangir758505@gmail.com>
2023-09-13 21:11:51 +0600

    update

--- pages/about/index.js
+++ pages/about/index.js
@@ -19,8 +19,8 @@ import Circles from "../../components/Circles";
 import { useState } from "react";
 import Avatar from "../../components/Avatar";
 
-import {motion} from "framer-motion";
-import {fadeIn} from "../../variants";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
 import CountUp from "react-countup";
 
 //  data
@@ -96,12 +96,12 @@ const aboutData = [
 ];
 
 const About = () => {
-  const [index, setIndex ] = useState(0);
+  const [index, setIndex] = useState(0);
   return (
     <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
       <Circles />
       {/* Avatar */}
-      <motion.div 
+      <motion.div
         variants={fadeIn('right', 0.2)}
         initial="hidden"
         animate="show"
@@ -111,23 +111,24 @@ const About = () => {
       </motion.div>
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         <div className="flex-1 flex flex-col justify-center">
-          <motion.h2 
+          <motion.h2
             variants={fadeIn('right', 0.2)}
             initial="hidden"
             animate="show"
             exit="hidden"
             className="h2">Captivating<span className="text-accent">stories</span> birth magnificent designs.</motion.h2>
+            // eslint-disable-next-line react/no-unescaped-entities
           <motion.p variants={fadeIn('right', 0.4)}
-        initial="hidden"
-        animate="show"
-        exit="hidden" className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">5 years ago, I began freelancing as a developer. Since then , I've done remote work for agencies, consulted dor startup, and collaborated on digital products for business and consumer use.</motion.p>
+            initial="hidden"
+            animate="show"
+            exit="hidden" className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">5 years ago, I began freelancing as a developer. Since then , I've done remote work for agencies, consulted dor startup, and collaborated on digital products for business and consumer use.</motion.p>
 
           {/* Counters */}
           <motion.div variants={fadeIn('right', 0.6)}
-        initial="hidden"
-        animate="show"
-        exit="hidden" className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8">
-            <div className="flex flex-1 xl:gap-x-6"> 
+            initial="hidden"
+            animate="show"
+            exit="hidden" className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8">
+            <div className="flex flex-1 xl:gap-x-6">
               {/* Experience */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
@@ -135,22 +136,22 @@ const About = () => {
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Years of experience</div>
               </div>
-               {/* Clients */}
-               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+              {/* Clients */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
                   <CountUp start={0} end={250} duration={5} />
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Satisfied Clients</div>
               </div>
-               {/* Project */}
-               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+              {/* Project */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
                   <CountUp start={0} end={560} duration={5} />
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Finished projects</div>
               </div>
-               {/* Project */}
-               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+              {/* Project */}
+              <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
                   <CountUp start={0} end={15} duration={5} />
                 </div>
@@ -160,14 +161,14 @@ const About = () => {
           </motion.div>
         </div>
         <motion.div variants={fadeIn('left', 0.2)}
-        initial="hidden"
-        animate="show"
-        exit="hidden" className="flex flex-col w-full xl:mx-w-[48%] h-[480px]">
+          initial="hidden"
+          animate="show"
+          exit="hidden" className="flex flex-col w-full xl:mx-w-[48%] h-[480px]">
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => (
-              <div 
-                key={itemIndex} 
-                className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} z-50 cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`} 
+              <div
+                key={itemIndex}
+                className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} z-50 cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
                 onClick={() => setIndex(itemIndex)}
               >
                 {item.title}
